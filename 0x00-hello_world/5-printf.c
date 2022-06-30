3f8dd46a81b653ab41748c038dc875a5856e40b8
Jacqoa <awulehande@gmail.com>
2022-06-30 14:45:37 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
+
 #include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description to Write a C program that prints exactly
- * with proper grammar, but the outcome is a piece of art
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-printf("with proper grammar, but the outcome is a pice of art,\n);
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
