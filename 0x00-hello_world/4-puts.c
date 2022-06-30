9dd601911e99ca6e9dbf32f8962f88784eb4a99c
Jacqoa <awulehande@gmail.com>
2022-06-30 12:51:03 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-int main (void)
-   [
+int main (void)[
       puts("\"Programming is like building a multilingualpuzzle");
-      return(0)
-   ]
-
-   
+      return(0);
+      ]
