0ae3cdd373f68aad99528ea51fd63908086c18d6
Jacqoa <awulehande@gmail.com>
2022-07-12 15:10:30 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+/**
+ * _strcpy -> function to copy string
+ * @dest: Destination value
+ * @src: source value
+ *
+ *Return: Pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++
+	{
+		dest[i] = src[i];
+	}
+
+	dest[++] = '\0';
+
+	return (dest);
+}
