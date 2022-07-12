ff68e63eb9c4afb9f6ff3d35f1bb06be6a21e4a9
Jacqoa <awulehande@gmail.com>
2022-07-12 08:58:17 -0700

    Task 1:Write a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_integer ->function that swaps the values of two integers
+ * @a: variable 1
+ * @b: variable 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
