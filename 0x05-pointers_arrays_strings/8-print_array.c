09d8fe6ad719809d50152a3f781029dc597f1dd0
Jacqoa <awulehande@gmail.com>
2022-07-12 20:25:16 -0700

    Task 8: function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *print_array -> prints n elements of an array of integers
- *@a: Arrays
- *@n: number of element of the array to be printed
+ * print_array -> prints n elements of an array of integers
+ * @a: Arrays
+ * @n: number of element of the array to be printed
  *
  * Return: void
  */
@@ -20,6 +20,7 @@ void print_array(int *a, int n)
 		{
 			printf(", ");
 		}
-		printf('\n');
 	}
+
+	printf("\n");
 }
