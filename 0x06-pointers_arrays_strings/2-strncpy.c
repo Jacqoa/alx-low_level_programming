6ace25a25fb3f7475fd923e8068a986c29cdd456
Jacqoa <awulehande@gmail.com>
2022-07-14 07:48:21 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> to concatenate two strings
+ * @dest: first param
+ * @src: Second param
+ * @n: Third param
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
