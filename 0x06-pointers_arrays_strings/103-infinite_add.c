f25b2184c4ecbc52be6765939cc08e2bde229df4
Jacqoa <awulehande@gmail.com>
2022-07-14 14:00:01 -0700

    Task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * infinite_add - C function that adds two numbers
+ * in string to a buffer
+ * Assumes that strings are never empty
+ * that nmbers will always be possitive
+ * Assumes that what is stored in the number string is digitals
+ * And, if the result can be stored in the buffer
+ * returns a pointer
+ * If result cannot be stored in the buffer, returns '0'
+ * @n1: first number
+ * @n2: second number
+ * @r: stored result
+ * @size_r: size of buffer
+ *
+ * Return: string
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
