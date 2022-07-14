e26d873f4e18aa86d64047e972fd617130c255cb
Jacqoa <awulehande@gmail.com>
2022-07-14 13:13:07 -0700

    Task 9: function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,9 +13,11 @@ void print_number(int n)
 	if (n < o)
 	{
 		_putchar('-');
-		num = num;
+		num = -num;
 	}
-	if ((num / 10) > 0);
-		print_number(num / 10);
+
+	if ((num / 10) > 0)
+	print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }
