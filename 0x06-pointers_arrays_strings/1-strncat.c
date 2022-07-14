d23a15b7b3917ddb138aeeddfb36d3025ce73d68
Jacqoa <awulehande@gmail.com>
2022-07-14 08:00:46 -0700

    Task 1: function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat -> function to concatenate two strings
+ * @dest: string to overwrite
+ * @src: string to copy
+ * @n: number
+ * Return: Return string
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	strncat(dest, src, n);
+	return (dest);
+}
