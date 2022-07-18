6a179b5740f2a38e4ee07c84e1f7f6c8ccd2f00b
Jacqoa <awulehande@gmail.com>
2022-07-18 09:01:40 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function to locate a string
+ * @s: source string
+ * @accept: accepted input
+ *
+ * Retiurn: Always
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (a[a])
+	{
+		b = 0;
+		while (accept[b])
+		{	
+			if (s[a] == accepted[b])
+			{
+				s += a;
+				return (s);
+			}
+		 	b++;
+		}
+		a++;
+	}
+
+	return ('\0');
+
+}
